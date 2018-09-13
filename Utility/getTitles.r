0a6b2dad3178a2fab48e15baa1617ff2ef50d8b0
Andrea Guzzo <andrea.guzzo92@gmail.com>
2018-09-13 18:56:01 +0200

    Created function for getting titles

--- Utility/getTitles.r
+++ Utility/getTitles.r
@@ -11,21 +11,31 @@ source("./Utility/utility.R")
 
 # environmentSettings()
 
-myTitles <- c("GOOG","AAPL","MSFT","AMZN","INTC")
-numberTitles <- ncol(myTitles)
-
 # mystocks <- new.env(hash=TRUE)
 # result <- getSymbols(myTitles,src="yahoo", auto.assign = TRUE, env=mystocks)
-
-getSymbols(myTitles, src="yahoo")
-
-ClosePrices <- do.call(merge, lapply(myTitles, function(x) Cl(get(x))))
-OpenPrices <- do.call(merge, lapply(myTitles, function(x) Op(get(x))))
-Volumes <- do.call(merge, lapply(myTitles, function(x) Vo(get(x))))
-Adjusted <- do.call(merge, lapply(myTitles, function(x) Ad(get(x))))
-
+getTitles <- function(myTitle,source){
+  
+  numberTitles <- ncol(myTitles)  
+  
+  getSymbols(myTitles, src=source)
+  
+  #TODO: capire quale valore utilizzare
+  ClosePrices <<- do.call(merge, lapply(myTitles, function(x) Cl(get(x))))
+  OpenPrices <<- do.call(merge, lapply(myTitles, function(x) Op(get(x))))
+  Volumes <<- do.call(merge, lapply(myTitles, function(x) Vo(get(x))))
+  Adjusted <<- do.call(merge, lapply(myTitles, function(x) Ad(get(x))))
+  
+  return(ClosePrices)
+  
+}
 
 #Capire cosa fare con i dati
 
+#Puoi usare differenti titoli, quelli che più ti interessano
+myTitles <- c("GOOG","AAPL","MSFT","AMZN","INTC")
+#Puoi utilizzare servizi differenti oltre a Yahoo
+#Se vuoi usare più servizi si può fare una funzione.
+source = "yahoo"
 
+getTitles(myTitles,source)
 
